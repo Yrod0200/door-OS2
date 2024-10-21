aac5d90ea164a89613a83c0c612a5bc13d60ef45
Yrod0200 <yurisantarosarodrigues3@gmail.com>
2024-10-21 00:41:42 -0300

    os changed a bit

--- main/.os.lua
+++ main/.os.lua
@@ -1,86 +1,77 @@
 local fs = require("filesystem")
 local cp = require("component")
 local c = require("computer")
-local e = require("event")
+local event = require("event")
 
-
+local gpu
 
 function have_gpu()
-  if cp.isAvailable("gpu") then
-    return true, cp.gpu
-  else 
-    return false
-  end
+    if cp.isAvailable("gpu") then
+        gpu = cp.gpu
+        return true
+    else 
+        return false
+    end
 end
 
-local success, gpu = have_gpu()
+if not have_gpu() then
+    c.crash("No GPU available.")
+end
+
+local w, h = gpu.getResolution()
 
 function cls()
-  while true do
-    local w, h = gpu.getResolution()
     gpu.setBackground(0x0000FF)
     gpu.fill(1, 1, w, h, " ")
-    coroutine.yield()
-  end
 end
 
 function get_date()
-    uptime = c.uptime() 
-    local w, h = gpu.getResolution()
-    free_mem = "  FREE RAM: " .. tostring(c.freeMemory()) 
-    msg = uptime .. free_mem
+    local uptime = c.uptime()
+    local free_mem = "  FREE RAM: " .. tostring(c.freeMemory())
+    local msg = uptime .. free_mem
     gpu.setForeground(0xFFFFFF)
-    gpu.set(w/2 - -#msg, 5, msg)
+    gpu.set(w / 2 - #msg / 2, 5, msg)
 end
+
 function door_os_name()
-    local w, h = gpu.getResolution()
-      gpu.setForeground(0xFFFFFF)
-      gpu.set(w -2 , h - 2, "D-OS/2")
+    gpu.setForeground(0xFFFFFF)
+    gpu.set(w - 7, h - 2, "D-OS/2")
 end
 
 function render()
-while true do
-    get_date()
-    door_os_name()
-    coroutine.yield()
-end
+    while true do
+        cls()
+        get_date()
+        door_os_name()
+        coroutine.yield()
+    end
 end
 
 function get_device_added()
     local _, device, type = event.pull("component_added")
     gpu.setForeground(0xFFFFFF)
-    msg = "Device " .. type .. "Connected."
+    local msg = "Device " .. type .. " connected."
     c.beep(750, 300)
-    gpu.set(w/2 -#msg , 2 - 2, msg)
+    gpu.set(w / 2 - #msg / 2, 2, msg)
     coroutine.yield()
 end
-
-g_d_add = coroutine.create(get_device_added)
 
 function events()
-while true do
-    os.sleep(0.0001)
-    g_d_add.resume()
-    coroutine.yield()
-end
-end
-cls_t = coroutine.create(cls)
-hello_t = coroutine.create(render)
-event_t = coroutine.create(events)
-function main()
-  while true do
-    local success, gpu = have_gpu()
-    if success then
-      os.sleep(0.0001)
-      coroutine.resume(event_t)
-      os.sleep(0.0001)
-      coroutine.resume(cls_t)
-      os.sleep(0.0001)
-      coroutine.resume(hello_t)
-    else
-      c.crash("No GPU available.")
+    while true do
+        get_device_added()
+        coroutine.yield()
     end
-  end
+end
+
+local render_t = coroutine.create(render)
+local event_t = coroutine.create(events)
+
+function main()
+    while true do
+        coroutine.resume(event_t)
+        coroutine.resume(render_t)
+        os.sleep(0.01)
+    end
 end
 
 main()
