8ab94587b012c7df7d08762c8dd10d39a7ee0da3
Yrod0200 <yurisantarosarodrigues3@gmail.com>
2024-10-21 14:30:35 -0300

    ava

--- main/.os.lua
+++ main/.os.lua
@@ -51,7 +51,7 @@ function render()
 end
 
 function event_touch()
-    local evname, _, x, y = event.pull(1)
+    local evname, _, x, y = event.pull(0.1)
     if evname == "touch" then
         local msg = "TOUCHED: " .. tostring(x) .. ", " .. tostring(y)
         set_text(0, 10, msg)
