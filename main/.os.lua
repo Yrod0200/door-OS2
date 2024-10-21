2c4646b058dc72b14261d4e77f7ce9452741d34b
Yrod0200 <yurisantarosarodrigues3@gmail.com>
2024-10-21 00:22:06 -0300

    commited

--- main/.os.lua
+++ main/.os.lua
@@ -1,7 +1,8 @@
 local fs = require("filesystem")
 local cp = require("component")
 local c = require("computer")
-
+local e = require("events")
+w, h = c.getResolution()
 
 function have_gpu()
   if cp.isAvailable("gpu") then
@@ -23,23 +24,54 @@ function cls()
 end
 
 function get_date()
-  while true do
+    uptime = c.uptime() 
+    free_mem = "  FREE RAM: " .. tostring(c.freeMemory()) 
+    msg = uptime .. free_mem
     gpu.setForeground(0xFFFFFF)
-    gpu.set(25, 5, "HELLO!")
-    coroutine.yield()
-  end
+    gpu.set(w/2 - -#msg, 5, msg)
+end
+function door_os_name()
+      gpu.setForeground(0xFFFFFF)
+      gpu.set(w -2 , h - 2, D-OS/2)
 end
 
-cls_t = coroutine.create(cls)
-hello_t = coroutine.create(get_date)
+function render()
+while true do
+    get_date()
+    door_os_name()
+    coroutine.yield()
+end
+end
 
+function get_device_added()
+    local _, device, type = event.pull("component_added")
+    gpu.setForeground(0xFFFFFF)
+    msg = "Device " .. type .. "Connected."
+    c.beep(750, 300)
+    gpu.set(w/2 -#msg , 2 - 2, msg)
+    coroutine.yield()
+end
+
+g_d_add = coroutine.create(get_device_added)
+
+function events()
+while true do
+    os.sleep(0.0001)
+    g_d_add.resume()
+end
+end
+cls_t = coroutine.create(cls)
+hello_t = coroutine.create(render)
+event_t = coroutine.create(events)
 function main()
   while true do
     local success, gpu = have_gpu()
     if success then
-      os.sleep(0.01)
+      os.sleep(0.0001)
+      coroutine.resume(event_t)
+      os.sleep(0.0001)
       coroutine.resume(cls_t)
-      os.sleep(0.01)
+      os.sleep(0.0001)
       coroutine.resume(hello_t)
     else
       c.crash("No GPU available.")
