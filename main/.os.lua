0d20449d5211bbac348c1265527e6cfad98fc98e
Yrod0200 <yurisantarosarodrigues3@gmail.com>
2024-10-21 14:11:03 -0300

    bruh2

--- main/.os.lua
+++ main/.os.lua
@@ -52,7 +52,7 @@ end
 
 function event_touch()
     local evname, _, x, y = event.pull(0.1, "touch")
-    fi evname == "touch" then
+    if evname == "touch" then
         msg = "TOUCHED: " .. tostring(x) .. ", " .. tostring(y)
         set_text(0, 10, msg)
     end
